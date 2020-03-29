680da8acd986f3f3ed71c8b000f044cf397a197e
Marcin Grobelak <marcin@code-craft.pl>
2020-03-29 19:39:13 +0200

    Further implementation of FrameTest, modifications in pom.xml (parametrized tests, weld-junit)

--- src/main/test/pl/code_craft/frame/FrameTest.java
+++ src/main/test/pl/code_craft/frame/FrameTest.java
@@ -1,14 +1,23 @@
 package pl.code_craft.frame;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class FrameTest {
 
@@ -51,6 +60,114 @@ class FrameTest {
 		frame.addRoll(10);
 		// then
 		assertFalse(frame.isSpare());
+	}
+
+	@ParameterizedTest
+	@MethodSource("possiblePairsWithSumLessThanTen")
+	void frameIsNotSpareWhenSumOfRolesNotEqualsTen(int first, int second) {
+		// when
+		frame.addRoll(first);
+		frame.addRoll(second);
+		// then
+		assertFalse(frame.isSpare());
+	}
+
+	@Test
+	void firstRollIsNotNullAfterFirstAddRollCall() {
+		// when
+		frame.addRoll(1);
+		// then
+		assertNotNull(frame.getFirstRoll());
+	}
+
+	@Test
+	void secondRollIsNotNullAfterSecondAddCallOnNonStrike() {
+		// when
+		frame.addRoll(1);
+		frame.addRoll(2);
+		// then
+		assertNotNull(frame.getSecondRoll());
+	}
+
+	@Test
+	void exceptionShouldBeThrownIfAddRollToStrike() {
+		// when
+		frame.addRoll(10);
+		// then
+		assertThrows(IllegalStateException.class, () -> frame.addRoll(1));
+	}
+
+	@Test
+	void exceptionShouldBeThrownIfAddRollToClosedFrame() {
+		// when
+		frame.addRoll(1);
+		frame.addRoll(2);
+		// then
+		assertThrows(IllegalStateException.class, () -> frame.addRoll(3));
+	}
+
+	@Test
+	void strikeIsFullPointer() {
+		// when
+		frame.addRoll(10);
+		// then
+		assertTrue(frame.isFullPointer());
+	}
+
+	@Test
+	void spareIsFullPointer() {
+		// when
+		frame.addRoll(2);
+		frame.addRoll(8);
+		// then
+		assertTrue(frame.isFullPointer());
+	}
+
+	@ParameterizedTest
+	@MethodSource("possiblePairsWithSumLessThanTen")
+	void frameWithSumOfRollsLestThanTenIsNotFullPointer(int first, int second) {
+		// when
+		frame.addRoll(first);
+		frame.addRoll(second);
+		// then
+		assertFalse(frame.isFullPointer());
+	}
+
+	static List<Arguments> possiblePairsWithSumLessThanTen() {
+		List<Arguments> args = new ArrayList<>();
+		for (int i = 0; i <= 9; i++) {
+			for (int j = 0; j <= 9; j++) {
+				if (i + j < 10) {
+					args.add(Arguments.of(i, j));
+				}
+			}
+		}
+		return args;
+	}
+
+	@Test
+	void sumeOfRollsReturnsNullIfFirstRollIsNull() {
+		assertNull(frame.getFirstRoll());
+		assertNull(frame.getSumOfRolles());
+	}
+
+	@Test
+	void sumeOfRollsReturnsNullIfSecondRollIsNull() {
+		// when
+		frame.addRoll(1);
+		// then
+		assertNull(frame.getSecondRoll());
+		assertNull(frame.getSumOfRolles());
+	}
+
+	@Test
+	void sumeOfRollsReturnsSumIfBothRollsAreNotNull() {
+		// when
+		frame.addRoll(1);
+		frame.addRoll(2);
+		// then
+		assertNotNull(frame.getSumOfRolles());
+		assertEquals(3, frame.getSumOfRolles());
 	}
 
 }
